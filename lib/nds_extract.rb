c888224f6d102d999c22043d6de9cfba41739345
remi.odufuye <remi.odufuye@gmail.com>
2019-10-21 22:35:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,15 +10,6 @@ def directors_totals(nds)
   
  
   result = {
-         "Stephen Spielberg"=>1357566430,
-        "Russo Brothers"=>2281002470,
-        "James Cameron"=>2571020373,
-        "Spike Lee"=>256624217,
-        "Wachowski Siblings"=>806180282,
-        "Robert Zemeckis"=>1273838385,
-        "Quentin Tarantino"=>662738268,
-        "Martin Scorsese"=>636812242,
-        "Francis Ford Coppola"=>509719258
         
   }
   
